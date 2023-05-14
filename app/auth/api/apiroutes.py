2fc88f4df405e345dbfb7763cc6640c4e01ba814
Brian Vo <Vobrian025@gmail.com>
2023-05-13 21:17:57 -0400

    auth

---
+++ app/auth/api/apiroutes.py
@@ -0,0 +1,163 @@
+from . import api
+from models import Product, Carts, Users, db
+from apiauthhelper import token_auth, basic_auth
+from flask import request, flash, url_for, redirect, render_template
+from flask_login import current_user
+
+# @app.route('/')
+# def homePage():
+#     all_products = Product.query.all()
+#     return render_template('index.html',all_products=all_products)
+
+@api.post('/signup')
+def signUpAPI():
+    data = request.json
+
+    username = data['username']
+    email = data['email']
+    password = data['password']
+
+    user = Users.query.filter_by(username = username).first()
+    if user:
+        return {
+            'status': 'not ok',
+            'message': 'Please choose a different username.'
+        }, 400
+    user = Users.query.filter_by(email = email).first()
+    if user:
+        return {
+            'status': 'not ok',
+            'message': 'That email is already in use.'
+        }, 400
+
+    user = Users(username, email, password)
+    user.saveToDB()
+    return {
+        'status': 'ok',
+        'message': "You have successfully created an account."
+    }, 201
+
+
+@api.post('/login')
+@basic_auth.login_required
+def loginAPI():
+    return {
+        'status': 'ok',
+        'message': "You have successfully logged in.",
+        'data': basic_auth.current_user().to_dict()
+    }, 200
+
+
+
+api.get('/product/<int:product_id>')
+@basic_auth.login_required
+def getProductAPI(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return {
+            'status':'ok',
+            'results': 1,
+            'product': product.to_dict()
+        }
+    else:
+        return {
+            'status' : 'not ok',
+            'message' : 'product does not exist'
+        }, 404
+    
+
+api.get('/additem/<int:product_id>')
+@basic_auth.login_required
+def getProductAPI(product_id):
+    product = product.query.get(product_id)
+    if not product:
+        return redirect(url_for("homePage"))
+    
+    user_id = current_user.id
+
+    cart_entry = Carts.query.filter(db.and_(Carts.user_id==user_id,Carts.product_id==product_id)).all()
+
+    if not cart_entry:
+        print("not carts")
+        new_entry = Carts(user_id,product_id,1)
+        new_entry.saveToDB()
+    else:
+        cart_entry[0].item_quantity += 1
+        cart_entry[0].saveToDB()
+    
+    flash(f"You successfully added {product.product_name} to your cart","success")
+    return redirect(url_for("showMyCart"))
+
+       
+api.get('removeitem/<int:product_id>')
+def getProductAPI(product_id):
+    product = product.query.get(product_id)
+
+
+@api.get('/checkout')
+@basic_auth.login_required
+def checkout():
+    cart_items = current_user.cart_items
+    if not cart_items:
+        flash("You have nothing in your cart, nothing to checkout","danger")
+        return redirect(url_for("showMyCart"))
+    
+    for cart_item in cart_items:
+        cart_item.deleteFromDB()
+
+    flash("Thanks for shopping with us, you've successfully checked out. Use used your credit we found on the dark web","success")
+    return redirect(url_for("homePage"))
+
+@api.get('/removeitem/<int:product_id>')
+@basic_auth.login_required
+def removeItemFromCart(product_id):
+    product = Product.query.get(product_id)
+
+    if not product:
+        flash("Product does not exist","danger")
+        return redirect(url_for("homePage"))
+    
+    user_id = current_user.id
+
+    cart_entry = Carts.query.filter(db.and_(Carts.user_id==user_id,Carts.product_id==product_id)).all()
+
+    if not cart_entry:
+        flash("This item is not in your cart, you can't remove it","danger")
+        return redirect(url_for("homePage"))
+    
+    cart_entry = cart_entry[0]
+    cart_quantity = cart_entry.item_quantity
+
+    if cart_quantity == 1:
+        cart_entry.deleteFromDB()
+        flash(f"You successfully removed {product.product_name}","success")
+        return redirect(url_for("showMyCart"))
+    else:
+        cart_entry.item_quantity -= 1
+        cart_entry.saveToDB()
+        flash(f"You successfully removed one {product.product_name}","success")
+        return redirect(url_for("showMyCart"))
+
+
+@api.get('/emptycart')
+@basic_auth.login_required
+def emptyCart():
+    cart_items = current_user.cart_items
+    if not cart_items:
+        flash("Your cart was already empty, nothing was removed","danger")
+        return redirect(url_for("showMyCart"))
+    
+    for cart_item in cart_items:
+        cart_item.deleteFromDB()
+
+    flash("You successfully removed all items from your cart","success")
+    return redirect(url_for("showMyCart"))
+
+@api.get('/cart')
+@basic_auth.login_required
+def showMyCart():
+
+    return render_template("cart.html")
+
+
+
---
+++ app/models.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from secrets import token_hex
+# Instantiate the database
+db = SQLAlchemy()
+
+class Users(db.Model,UserMixin):
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(100),nullable = False,unique=True)
+    password = db.Column(db.String,nullable=False)
+    first_name = db.Column(db.String(50), nullable = False)
+    last_name = db.Column(db.String(50))
+    date_joined = db.Column(db.DateTime,nullable = False, default=datetime.utcnow())
+    apitoken = db.Column(db.String, unique=True)
+
+
+    def __init__(self, email, password, first_name,last_name):
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.apitoken = token_hex(16) 
+
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'email' : self.email,
+            'password' : self.password,
+            'first_name' : self.first_name,
+            'last_name' : self.last_name,
+            'apitoken' : self.apitoken
+        }
+
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def deleteFromDB(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Product(db.Model):
+    id = db.Column(db.Integer, Primary_key=True)
+    product_name = db.Column(db.String(100), nullable = False, unique=True)
+    price = db.Column(db.Float, nullable = False)
+    description = db.Column(db.String(300), nullable = False)
+    image_url = db.Column(db.String)
+
+    def __init__(self, product_name, price, description, image_url):
+        self.product_name = product_name
+        self.price = price
+        self.description = description
+        self.image_url = image_url
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def deleteFromDB(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def to_dict(self):
+        return {
+            'id' : self.id,
+            'product_name' : self.product_name,
+            'price' : self.price,
+            'description' : self.description,
+            'image_url' : self.image_url
+        }
+    
+class Carts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_quantity = db.Column(db.Integer,nullable=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey(Users.id),nullable=False)
+    user = db.relationship("Users",back_populates="cart_items",foreign_keys=[user_id])
+    
+    product_id = db.Column(db.Integer, db.ForeignKey(Products.id),nullable=False)
+    product = db.relationship("Products",back_populates="associated_carts",foreign_keys=[product_id])
+    
+    def __init__(self, user_id, product_id, item_quantity):
+        self.user_id = user_id
+        self.product_id = product_id
+        self.item_quantity = item_quantity
+
+    def saveToDB(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def deleteFromDB(self):
+        db.session.delete(self)
+        db.session.commit()
+    
