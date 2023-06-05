f4a0a992eaa6050bc1ec22c050ab27b232957b37
Brian Vo <Vobrian025@gmail.com>
2023-06-05 00:16:05 -0400

    done

--- app/api/apiroutes.py
+++ app/api/apiroutes.py
@@ -1,6 +1,6 @@
 from . import api
 from ..models import Product, Carts, Users, db
-from .apiauthhelper import token_auth, basic_auth
+from .apiauthhelper import token_auth, basic_auth, token_auth_required
 from flask import request, flash, url_for, redirect, render_template
 from flask_login import current_user
 import stripe
@@ -12,8 +12,8 @@ stripe.api_key = os.environ.get('STRIPE_API_KEY')
 
 #SHOWS SINGLE PRODUCT WHEN CLICKED ON FIX THIS 
 @api.get('/Singleproduct/<int:product_id>')
-@basic_auth.login_required
 def SingleproductAPI(product_id):
+    print("IM HERE")
     product = Product.query.get(product_id)
     if product:
         return {
@@ -26,21 +26,20 @@ def SingleproductAPI(product_id):
             'status' : 'not ok',
             'message' : 'product does not exist'
         }, 404
-    
 
-@api.get('/addToCart/<int:product_id>')
-# @basic_auth.login_required
-@token_auth.login_required
-def addToCartAPI(product_id):
-    user = token_auth.current_user()
-    data = request.json
 
-    product_id = data['product_id']
+@api.post('/addToCart/<int:product_id>')
+@token_auth_required
+def addToCartAPI(user, product_id):
+    # user = token_auth.current_user()
+    # data = request.json
+
+    # product_id = data['product_id']
     product = Product.query.get(product_id)
 
     if product:
 
-        cart_item = Carts(user.id, product.id)
+        cart_item = Carts(user.id, product.id, 1)
         cart_item.saveToDB()
 
         return {
@@ -52,10 +51,15 @@ def addToCartAPI(product_id):
             'status': 'not ok',
             'message': 'That product does not exist!'
         }
-#SHOWS PRODUCTS FIX THIS
-@api.get('Singleproduct/<int:product_id>')
-def ProductsAPI(product_id):
-    product = Product.query.get(product_id)
+
+@api.get('/showProducts')
+def showProducts():
+    products = Product.query.all()
+    if products:
+        return {
+            'status': 'ok',
+            'products': [product.to_dict() for product in products]
+        }
 
 
 # @api.get('/checkout')
@@ -72,12 +76,11 @@ def ProductsAPI(product_id):
     # flash("Thanks for shopping with us, you've successfully checked out. Use used your credit we found on the dark web","success")
     # return redirect(url_for("homePage"))
 
-@api.get('/removeFromCart/<int:product_id>')
-# @basic_auth.login_required
-@token_auth.login_required
-def removeFromCartAPI(product_id):
+@api.post('/removeFromCart/<int:product_id>')
+@token_auth_required
+def removeFromCartAPI(user, product_id):
     product = Product.query.get(product_id)
-    item = Carts.query.filter_by(user_id=Users.id).filter_by(product_id=product_id).first()
+    item = Carts.query.filter_by(user_id=user.id, product_id=product_id).first()
     if item:
         item.deleteFromDB()
         return {
@@ -91,39 +94,32 @@ def removeFromCartAPI(product_id):
 
 
 
-@api.get('/emptycart')
-@basic_auth.login_required
-def emptyCartAPI():
-    cart_items = current_user.cart_items
-    if not cart_items:
+@api.post('/emptycart')
+@token_auth_required
+def emptyCartAPI(user):
+    cart_items = Carts.query.filter_by(user_id=user.id).all()
+    if cart_items:
+        for item in cart_items:
+            item.deleteFromDB()
         return {
             'status': 'ok',
             'message': "Entire cart has been deleted"
         }
-    return {
-        'status': 'not ok',
-        'message': 'There is nothing to remove from cart'
-    }
-        
-    
-    # for cart_item in cart_items:
-    #     cart_item.deleteFromDB()
-
+    else:
+        return {
+            'status': 'not ok',
+            'message': 'There is nothing to remove from cart'
+        }
 
 @api.get('/cart')
-@basic_auth.login_required
-def CartAPI():
-    user = token_auth.current_user()
+@token_auth_required
+def CartAPI(user):
     return {
         'status': 'ok',
         'cart': [Product.query.get(c.product_id).to_dict() for c in Carts.query.filter_by(user_id=user.id).all()]
     }
 
-#return redirect (Cart)
-
-
-
-stripe.apikey = os.environ.get('STRIPE_API_KEY')
+stripe.api_key = os.environ.get('STRIPE_API_KEY')
 @api.post('/checkout')
 def checkout():
     try:
@@ -138,8 +134,8 @@ def checkout():
             line_items=[{
                 "price_data": {
                     "currency": "usd",
-                    "product_data": {"name": "IPHONE"},
-                    "unit_amount": 1000,
+                    "product_data": {"name": "WATCH"},
+                    "unit_amount": 800,
                     "tax_behavior": "exclusive",
                 },
                 'quantity':1
